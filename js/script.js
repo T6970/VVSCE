2dede7ac615e8f3ca7c5cf2d4aeeb5fd42b46d1c
T6970 <tenaldkyf315@gmail.com>
2025-11-16 16:50:01 +0800

    Add logic for tab buttons

--- js/script.js
+++ js/script.js
@@ -1,9 +1,34 @@
-import { insertTextAtCursor, isCursorIn } from "./insert.js";
 import { appendSnippets } from "./snippet.js";
 
+let tab = "html";
 
 const sidebar    = document.getElementById("sidebar");
 const snippetBox = document.getElementById("list");
 const editor     = document.getElementById("editor");
 
 appendSnippets('/snippets/js.json', snippetBox);
+
+const htmlTab    = document.getElementById("html-tab"   );
+const cssTab     = document.getElementById("css-tab"    );
+const jsTab      = document.getElementById("js-tab"     );
+const previewTab = document.getElementById("preview-tab");
+
+/**
+ * Initialize tabs.
+ * @param {*} tabButton 
+ * @param {*} toTab 
+ */
+const initTab = (tabButton, toTab) => {
+  tabButton.addEventListener("click", () => {
+    tab = toTab;
+  })
+};
+
+initTab(htmlTab, "html");
+initTab(cssTab,  "css" );
+initTab(jsTab,   "js"  );
+
+previewTab.addEventListener("click", () => {
+
+})
+

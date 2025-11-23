c41178e99f73975426bfab410e999519955e7327
T6970 <tenaldkyf315@gmail.com>
2025-11-23 21:44:43 +0800

    Rewrite and add blockstart and blockend

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-const sidebar    = document.getElementById("sidebar");
-const snippetBox = document.getElementById("list");
-const editor     = document.getElementById("editor");
+import { lang } from "../snippets/js.js"
+import { insertTextAtCursor } from "./insert.js";
+
+const editor = document.getElementById("editor");
+
+editor.addEventListener("keydown", (e) => {
+  if (e.key === lang.config.blockStart) {
+    e.preventDefault()
+    insertTextAtCursor(`${lang.config.blockStart}\n${" ".repeat(lang.config.indent+1)}\n${lang.config.blockEnd}`)
+  }
+})

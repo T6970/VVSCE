b94f3c1f32ed91de0bd884bda95d769418462fda
T6970 <tenaldkyf315@gmail.com>
2025-12-16 18:15:34 +0800

    Add file button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 import { Nqde } from 'https://cdn.jsdelivr.net/gh/T6970/Nqde@main/src/index.js'
 
 Nqde.newCard("editor")
+Nqde.resizeCard("editor", "800", "600")
+Nqde.moveCard("editor", "20", "100")
 Nqde.setContent("editor", '<div contenteditable="true">Write code here</div>')
+Nqde.newCard("file")
+Nqde.resizeCard("file", "15", "5")
+Nqde.moveCard("file", "20", "20")
+Nqde.setContent("file", 'File')
+Nqde.listenFor("file", "click", () => {
+  
+})

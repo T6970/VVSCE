7855632cb038ddba49d6b039ffe4d307585d1a00
T6970 <tenaldkyf315@gmail.com>
2025-12-26 12:20:50 +0800

    Fixing loadConfig()

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@ const loadConfig = async (filename) => {
   try {
     const raw = await fetch(filename)
     if (!raw.ok) throw new Error(`Failed to fetch ${filename}`)
-    const lang = raw.json
+    
+    const lang = await raw.json()
     console.log(lang)
     return lang
   } catch (e) {
     console.error(e)
   }
 }
+
+let lang
+lang = loadConfig("../conf/js.json")

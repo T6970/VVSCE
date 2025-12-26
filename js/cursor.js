ffd70294bc632c724e36017141ac93810d6cf101
T6970 <tenaldkyf315@gmail.com>
2025-12-26 18:40:14 +0800

    Implemented keyOverride

---
+++ js/cursor.js
@@ -0,0 +1,8 @@
+export const insert = (text) => {
+  const sel = window.getSelection()
+  if (!sel.rangeCount) return
+
+  const range = sel.getRangeAt(0)
+  range.insertNode(document.createTextNode(text))
+  range.collapse(false)
+}
--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
+import { insert } from "./cursor.js"
+
 const loadConfig = async (filename) => {
   try {
-    const raw = await fetch(filename)
+    const raw = await fetch(`../conf/${filename}.json`)
     if (!raw.ok) throw new Error(`Failed to fetch ${filename}`)
     
     const lang = await raw.json()
@@ -11,5 +13,22 @@ const loadConfig = async (filename) => {
   }
 }
 
-let lang
-lang = loadConfig("../conf/js.json")
+
+const main = async () => {
+  const editor = document.getElementById("editor")
+
+  let lang
+  lang = await loadConfig("js")
+
+  for (const [original, replace] of Object.entries(lang.keyOverride)) {
+    editor.addEventListener("keydown", (e) => {
+      if (e.key === original) {
+        e.preventDefault
+        console.log(replace)
+        insert(replace)
+      }
+    })
+  }
+}
+
+main()

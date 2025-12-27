d2c0366eeded2a561a9bbfdb21df06efbd27ff37
T6970 <tenaldkyf315@gmail.com>
2025-12-27 13:05:41 +0800

    Adding insertAfter

--- js/cursor.js
+++ js/cursor.js
@@ -6,3 +6,15 @@ export const insertBefore = (text) => {
   range.insertNode(document.createTextNode(text))
   range.collapse(false)
 }
+
+export const insertAfter = (text) => {
+  const sel = window.getSelection()
+  if (!sel.rangeCount) return
+
+  const range = sel.getRangeAt(0)
+  range.insertNode(document.createTextNode(text))
+  range.setStartAfter(textNode);
+  range.collapse(true);
+  sel.removeAllRanges();
+  sel.addRange(range);
+}

c4193ef2b05588daf71e895779e450c4ec25ce86
T6970 <tenaldkyf315@gmail.com>
2026-01-01 19:04:32 +0800

    Added checks of presense of config

--- js/script.js
+++ js/script.js
@@ -2,33 +2,40 @@ import { insertAfter, insertBefore } from "./cursor.js"
 import { loadConfig } from "./loader.js"
 
 const main = async () => {
+  
   const editor = document.getElementById("editor")
 
   let lang
   lang = await loadConfig("js")
   console.log(lang)
 
-  for (const [original, {before, after}] of Object.entries(lang.keyOverride)) {
+  if (lang.keyOverride) {  
+    for (const [original, {before, after}] of Object.entries(lang.keyOverride)) {
+      editor.addEventListener("keydown", (e) => {
+        if (e.key === original) {
+          e.preventDefault()
+          insertBefore(before)
+          insertAfter(after)
+        }
+      })
+    }
+  }
+
+  if (lang.tabSize) {
     editor.addEventListener("keydown", (e) => {
-      if (e.key === original) {
-        e.preventDefault()
-        insertBefore(before)
-        insertAfter(after)
+      if (e.key !== "Enter") return
+      for (const indentChar of lang.indentChars) {
+        const cursorPos = editor.selectionStart
+        const beforeLength = editor.innerText.slice(0,cursorPos)
+        if (editor.innerText.slice((beforeLength - indentChar.length), cursorPos) === indentChar) {
+          insertBefore(`\n${" ".repeat(lang.tabSize)}`)
+          insertAfter("\n")
+        }
       }
     })
   }
 
-  editor.addEventListener("keydown", (e) => {
-    if (e.key !== "Enter") return
-    for (const indentChar of lang.indentChars) {
-      const cursorPos = editor.selectionStart
-      const beforeLength = editor.innerText.slice(0,cursorPos)
-      if (editor.innerText.slice((beforeLength - indentChar.length), cursorPos) === indentChar) {
-        insertBefore(`\n${" ".repeat(lang.tabSize)}`)
-        insertAfter("\n")
-      }
-    }
-  })
+
 }
 
 document.addEventListener("DOMContentLoaded", () => {
